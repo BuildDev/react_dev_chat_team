796ff698e767633fd5e50eeafa7941bff94df595
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2020-05-10 15:20:16 +0100

    install all third-party-library and add router

---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Resgister extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Resgister</h1>
+      </div>
+    );
+  }
+}
+
+export default Resgister;
