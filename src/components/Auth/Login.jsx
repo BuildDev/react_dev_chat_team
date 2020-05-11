dab2a2eeb8aec5b2896af94c960af26f6168d35e
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2020-05-11 18:22:23 +0100

    login v1

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
+import './login.css';
 
-class Login extends Component {
-    state = {}
+import { loginWithGoogle } from './login.actions';
+
+class LoginContainer extends Component {
+    login() {
+        this.props.firebase.login({ provider: 'google', type: 'popup' });
+    }
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="center">
+                <h1 className="text-center">Login to get started...</h1>
+                <img src={process.env.PUBLIC_URL + "/google_plus_icon.png"} width="200" height="200" alt="Google Login" onClick={() => this.login()} />
             </div>
         );
     }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({ loginWithGoogle }, dispatch);
+};
+
+LoginContainer.propTypes = {
+    loginWithGoogle: PropTypes.func.isRequired,
+};
+
+LoginContainer.contextTypes = {};
+
+const wrappedLogin = firebaseConnect()(LoginContainer);
+
+export let Login = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(wrappedLogin);
---
+++ src/components/Auth/LoginReducer.js
@@ -0,0 +1,17 @@
+// Import Actions
+import { LOGIN_SUCCESS } from './login.actions';
+
+// Initial State
+export const loginInitialState = {
+    isLoggedIn: false,
+    user: {}
+};
+
+export const LoginReducer = (state = loginInitialState, action) => {
+    switch (action.type) {
+        case LOGIN_SUCCESS:
+            return Object.assign({}, state, { isLoggedIn: true, user: action.payload });
+        default:
+            return state;
+    }
+};
