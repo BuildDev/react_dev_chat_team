1ed8483e44f3d517387df9bb8c73deef9034f3ea
Achref Rouabeh <42032975+BuildDev@users.noreply.github.com>
2025-05-15 11:36:40 +0200

    Update Firebase-utils.js

--- src/firebase/Firebase-utils.js
+++ src/firebase/Firebase-utils.js
@@ -4,14 +4,14 @@ import "firebase/database";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyBpafNsmn9nOXEl7cgzCYwPO0e8V1bDoBs",
-  authDomain: "react-dev-chat-team.firebaseapp.com",
-  databaseURL: "https://react-dev-chat-team.firebaseio.com",
-  projectId: "react-dev-chat-team",
-  storageBucket: "react-dev-chat-team.appspot.com",
-  messagingSenderId: "834554456",
-  appId: "1:834554456:web:e78a77b86244eab054982d",
-  measurementId: "G-4T9WL1H9VW",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
